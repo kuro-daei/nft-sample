ff2a5abea69185f693bb1bf6a7038be47aec5584
Eiji Kuroda <e@96d.jp>
2022-02-24 00:15:13 +0900

    upd

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,12 @@
-const hre = require("hardhat");
 async function main() {
-  const nftFactory = await hre.ethers.getContractFactory("NFT");
+  const nftFactory = await ethers.getContractFactory('NFT');
   const nft = await nftFactory.deploy();
   await nft.deployed();
-  console.log("NFT deployed to:", nft.address);
+  console.log('NFT deployed to address:', nft.address);
 }
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
     process.exit(1);
   });
